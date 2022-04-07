af817fafaedc7b84d561a171bfb22d02c2e3c5f2
Eyosiwk <eyoyapi@yahoo.com>
2022-04-07 11:13:37 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  *main - entry point
  *
- *retun: 0 after printing the function
+ *Retun: 0 after printing the function
  */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
 return (0):
+}
