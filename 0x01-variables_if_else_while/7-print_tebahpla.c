af83e333e94327dd746722534fecd4458065c53e
Eyosiwk <eyoyapi@yahoo.com>
2022-04-09 12:15:34 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+Einclude <stdio.h>
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
