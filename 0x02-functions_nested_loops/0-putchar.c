837d37cd7d84efe20fe15655c8bff92846d07884
Eyosiwk <eyoyapi@yahoo.com>
2022-04-13 11:07:03 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * @c: the caracter to print
  * 
  * Return: on success 1.
- * on error -1 is returned, and errno is set appropriately.
+ * on error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
