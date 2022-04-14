473cad7153ed221e02880b90928c6e258c92c594
Eyosiwk <eyoyapi@yahoo.com>
2022-04-14 11:34:24 -0700

    Task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,14 +13,14 @@ float tot_sum;
 
 while (1)
 {
-fibsum = fib2+ fib2;
+fibsum = fib1 + fib2;
 if (fibsum > 4000000)
 break;
 
 if ((fibsum % 2) == 0)
 tot_sum += fibsum;
 
-fib2 = fib2;
+fib1 = fib2;
 fib2 = fibsum;
 }
 printf("%.0f\n", tot_sum);
