3a8bd0487c6f3f947e1a06b2dc1fa2fbc8fbe605
Eyosiwk <eyoyapi@yahoo.com>
2022-04-21 14:48:11 -0700

    Task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,9 +13,9 @@ char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 
-for (x = 0; s[x] != '\0'; x++)
+for (x = 0; s[x] != '0'; x++)
 {
-for (y = 0; input[y] != '\0'; y++)
+for (y = 0; input[y] != '0'; y++)
 {
 if (s[x] == input[y])
 {
