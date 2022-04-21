d9de719f87af590d254c5d460e3b1de0835db4ed
Eyosiwk <eyoyapi@yahoo.com>
2022-04-21 12:00:55 -0700

    Task 5

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (success)
  */
 
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 
 {
 
@@ -15,7 +15,7 @@ void reverse_array(int *a, int n);
 	for (x = 0; x < (n / 2); x++)
 	{
 		y = a[x];
-		a[x] = a[n - x - 1);
+		a[x] = a[n - x - 1];
 		a[n - x - 1] = y;
 	}
 }
