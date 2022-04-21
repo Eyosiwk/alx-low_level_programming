5a0ca8ff09f3b06025eddd73684c09bcbdbb6350
Eyosiwk <eyoyapi@yahoo.com>
2022-04-21 14:12:35 -0700

    Task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,7 @@
-#include <stdio.h"
+#include <stdio.h>
 #include "main.h"
 /**
- * * print_number - prints an integer
+ * print_number - prints an integer
  * @n: number to be printed
  * Return: Always 0 (Success)
  */
