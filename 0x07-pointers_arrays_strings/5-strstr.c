bc25b021a531048f86c3c4046615b755492aa160
Eyosiwk <eyoyapi@yahoo.com>
2022-04-26 04:59:49 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,21 +20,3 @@ haystack++;
 }
 return (0);
 }
-/
- * coincidence - defines if the string b is inside a
- * @a: source string
- * @b: string to be searched
- * Return: 1 if there is coincidence, otherwise 0
- */
-int coincidence(char *a, char *b)
-{
-while (*b && *b == *a)
-{
-b++;
-a++;
-}
-if (*b == '\0')
-return (1);
-else
-return (0);
-}
