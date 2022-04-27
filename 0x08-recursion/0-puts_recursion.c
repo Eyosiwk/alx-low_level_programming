71343da13a819a7568fdf13080955b5318139798
Eyosiwk <eyoyapi@yahoo.com>
2022-04-27 09:58:35 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,13 @@
  * @s: string
  * Return: Always (0)
  */
-void_puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
-if (*s == '\0')
-_putchar('\n');
-else
-{
-_putchar(*s);
-_puts_recursion(++s);
+  if (*s == '\0')
+    _putchar('\n');
+  else
+  {
+    _putchar(*s);
+    _puts_recursion(++s);
+  }
 }
-}
-
