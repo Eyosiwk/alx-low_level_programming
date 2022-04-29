5440f3337922ceb0666ca97d9d16c1acbca0bda0
Eyosiwk <eyoyapi@yahoo.com>
2022-04-29 12:16:59 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+  int i;
+
+  printf("argc = %\n", argc);
+  printf("Let's see what is in argv[]\n");
+
+  for(i = 0; i < argc; i++)
+  {
+
+	  printf("argv[%d] = %s\n", i, argv[i]);
+  }
+ 
+	  return 0;
+}
+
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0x0A._C-rg_argv
