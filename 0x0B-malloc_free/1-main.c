f59889f318373b01767e90e0f3456b1bfb0d7ac4
Eyosiwk <eyoyapi@yahoo.com>
2022-05-03 11:00:49 -0700

    task 1

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -1,23 +1,55 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
+ * _strlen - returns the lenght of a string
  *
- * Return: Always 0.
+ *@s: string
+ *
+ *Return: length of string
  */
-int main(void)
-{
-    char *s;
 
-    s = _strdup("Holberton");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+int _strlen(char *s)
+{
+
+char *p = s;
+
+while (*s)
+
+{
+s++;
 }
+
+return (s - p);
+}
+
+/**
+ *_strdup - returns a pointer to a newly allocated space in memory
+ *
+ *@str: string to copy
+ *
+ *Return: copied string
+ */
+
+char *_strdup(char *str)
+{
+int i, l;
+char *copy;
+
+if (!str)
+return (0);
+
+l = _strlen(str);
+copy = malloc(sizeof(char) * l + 1);
+
+if (!copy)
+return (0);
+
+for (i = 0; i < l; i++)
+copy[i] = str[i];
+
+copy[i] = 0;
+
+return (copy);
+}
+
