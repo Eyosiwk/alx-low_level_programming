7417a5a3c9931c3ba2dad83a8e2972c9d4e80c38
Eyosiwk <eyoyapi@yahoo.com>
2022-05-09 10:56:30 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *struct dog - dog
+ *
+ *@name: dog's name
+ *
+ *@age: how old the dog is
+ *
+ *@owner: who owns the dog
+ */
+
+typedef struct dog
+{
+
+char *name;
+float age;
+char *owner;
+
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
